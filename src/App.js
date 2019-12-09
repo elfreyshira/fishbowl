333dcfa26d25c90c1694be272114df961ed06244
Elfrey Shira <elf.shira@gmail.com>
2019-12-09 19:25:17 +0700

    set up the start page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import _ from 'lodash'
 
-function App() {
+import logo from './logo.svg'
+import './App.css'
+
+
+function Button (props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button className="button" {...props}>
+        {props.children}
+    </button>
+  )
 }
 
-export default App;
+
+function submitNewEntry(evt, entry, listOfEntries, setListOfEntries, setEntry) {
+  evt.preventDefault()
+  if (!entry) return false
+
+  setListOfEntries([...listOfEntries, entry])
+  setEntry('')
+}
+
+
+function StartScreen ({changePage, listOfEntries, setListOfEntries}) {
+  const [entry, setEntry] = React.useState('')
+
+  return (
+    <div className="App">
+      <form onSubmit={evt => submitNewEntry(evt, entry, listOfEntries, setListOfEntries, setEntry)}>
+        <label>
+          New entry:
+          <input
+            type="text"
+            value={entry}
+            onChange={evt => setEntry(evt.target.value)}
+          />
+        </label>
+        <Button type="submit">Submit Entry</Button>
+      </form>
+
+      {
+      // put back in if you want to see the list of entries
+      //   <ul>
+      //   {listOfEntries.map((item)=><li>{item}</li>)}
+      // </ul>
+      }
+      <div>
+        Number of entries: {listOfEntries.length}
+      </div>
+
+      <Button onClick={() => changePage('play')}>Start Game</Button>
+    </div>
+  )
+}
+
+
+function PlayScreen ({listOfEntries}) {
+  return (
+    <div>PLAY BALL!</div>
+  )
+}
+
+function App () {
+  const [page, changePage] = React.useState('start')
+  const [listOfEntries, setListOfEntries] = React.useState([])
+
+  if (page === 'start') {
+    return <StartScreen {...{changePage, listOfEntries, setListOfEntries}} />
+  }
+  else if (page === 'play') {
+    return <PlayScreen />
+  }
+  else {
+    return <div>nothing</div>
+  }
+  
+}
+
+export default App

1503fa2039438af807f7633c089a61afd0c8e28a
Elfrey Shira <elf.shira@gmail.com>
2019-12-09 17:53:42 +0700

    Updates

---
+++ precache-manifest.f111ceaab97cfab5dc18b5cfeebd7f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0b4a377eb4c6f7d8f41a0f2d7475f0",
+    "url": "/fishbowl/index.html"
+  },
+  {
+    "revision": "76d0c725ef6ac36fe670",
+    "url": "/fishbowl/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5819e923b96a46cf57b2",
+    "url": "/fishbowl/static/js/2.e3d7443b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fishbowl/static/js/2.e3d7443b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76d0c725ef6ac36fe670",
+    "url": "/fishbowl/static/js/main.e3ee5f72.chunk.js"
+  },
+  {
+    "revision": "22cd9e3c219cb3beb8e8",
+    "url": "/fishbowl/static/js/runtime-main.05f55954.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fishbowl/static/media/logo.5d5d9eef.svg"
+  }
+]);
